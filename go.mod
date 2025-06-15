e85a6031aaca1d8641bbd08dacef627dccf96548
andreas19 <andreas19@posteo.eu>
2025-06-15 07:46:28 +0200

    Add Iter()

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
 module github.com/andreas19/maps
 
-go 1.20
+go 1.23
---
+++ iter.go
@@ -0,0 +1,14 @@
+package maps
+
+import "iter"
+
+// Iter returns an iterator over key-value pairs from m.
+func Iter[K comparable, V any](m map[K]V) iter.Seq2[K, V] {
+	return func(yield func(K, V) bool) {
+		for k, v := range m {
+			if !yield(k, v) {
+				return
+			}
+		}
+	}
+}
